189b41958176c8965d42659bbefaac5f728b3f2e
Jules Portier <juportie@z2r9p1.42lyon.fr>
2025-01-13 11:22:29 +0100

    feat: use ft_printf in place of standard printf

--- minitalk.h
+++ minitalk.h
@@ -1,22 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juportie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/10 14:07:21 by juportie          #+#    #+#             */
-/*   Updated: 2025/01/10 17:16:38 by juportie         ###   ########.fr       */
+/*   Created: 2025/01/13 11:16:46 by juportie          #+#    #+#             */
+/*   Updated: 2025/01/13 11:17:33 by juportie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
-
-int	main(int argc, char **argv)
-{
-	(void)argv;
-	if (argc != 1)
-		return (-1);
-	ft_printf("%d\n", getpid());
-	return (0);
-}
+#include <unistd.h>
+#include "lib/ft_printf/ft_printf.h"
