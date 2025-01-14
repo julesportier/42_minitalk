31a43338a08e923470e84039b0c4d482e220dca9
Jules Portier <juportie@z2r9p1.42lyon.fr>
2025-01-14 13:12:22 +0100

    feat: client stream char to server

--- client.c
+++ client.c
@@ -12,12 +12,44 @@
 
 #include "minitalk.h"
 
-int	main(int argc, char **argv)
+static int	stream_char(int pid, char c)
 {
-	if (argc != 2)
-		return (-1);
-	if (argv[1] > 0)
-		kill(ft_atoi(argv[1]), SIGKILL);
+	int	i;
+	int	err;
+
+	i = 0;
+	while (i < 8)
+	{
+		if (c % 2 != 0)
+			err = kill(pid, SIGUSR1);
+		else
+			err = kill(pid, SIGUSR2);
+		if (err == -1)
+			return (-1);
+		c = c >> 1;
+		i++;
+		usleep(1000);
+	}
 	return (0);
 }
-	
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+
+	pid = ft_atoi(argv[1]);
+	if (argc != 3)
+		return (-1);
+	if (pid > 0)
+	{
+		//kill(pid, argv[2][0]);
+		stream_char(pid, argv[2][0]);
+		//if (sigemptyset(sigset) == -1)
+		//	return (-1);
+		//if (sigaddset(sigset, SIGUSR1));
+		//	return(-1);
+	}
+	else
+		ft_printf("wrong signal (SIGUSR1 == 10; SIGUSR2 == 12)\n");
+	return (0);
+}
--- server.c
+++ server.c
@@ -12,16 +12,33 @@
 
 #include "minitalk.h"
 
+void	print_sig(int sig)
+{
+	if (sig == SIGUSR1)
+		ft_printf("1");
+	else if (sig == SIGUSR2)
+		ft_printf("0");
+	else
+		ft_printf("unknown signal\n");
+}
+
 int	main(int argc, char **argv)
 {
+	struct sigaction	sigact;
+	//sigset_t	*sigmask;
+
 	(void)argv;
+	//sigmask = init_sig_mask();
+	sigact.sa_handler = &print_sig;
+	//sigact.sa_mask = *sigmask;
 	if (argc != 1)
 		return (-1);
+	ft_printf("%d\n", getpid());
 	while (1)
 	{
-		ft_printf("%d\n", getpid());
-		if(sleep(1))
-			return (-1);
+		sigaction(SIGUSR1, &sigact, NULL);
+		sigaction(SIGUSR2, &sigact, NULL);
+		pause();
 	}
 	return (0);
 }
